9891ef257a4ade890456292babc168cffe2d024b
Apollo <nitsoramo@gmail.ocm>
2018-10-09 10:42:09 +0500

    On branch shoppinglist  Changes to be committed: 	modified:   app/Http/Controllers/Auth/LoginController.php 	modified:   app/Http/Controllers/Auth/RegisterController.php 	new file:   app/Http/Controllers/HomeController.php 	new file:   app/Item.php 	new file:   app/List.php 	modified:   app/User.php 	modified:   database/migrations/2014_10_12_000000_create_users_table.php 	modified:   database/migrations/2014_10_12_100000_create_password_resets_table.php 	new file:   database/migrations/2018_10_09_051429_create_items_table.php 	new file:   database/migrations/2018_10_09_051449_create_lists_table.php 	new file:   resources/views/auth/login.blade.php 	new file:   resources/views/auth/passwords/email.blade.php 	new file:   resources/views/auth/passwords/reset.blade.php 	new file:   resources/views/auth/register.blade.php 	new file:   resources/views/home.blade.php 	new file:   resources/views/layouts/app.blade.php 	modified:   routes/web.php

--- database/migrations/2018_10_09_051449_create_lists_table.php
+++ database/migrations/2018_10_09_051449_create_lists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateListsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('lists', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            //$table->string('email')->unique();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title', 155)->default('');
             $table->timestamps();
         });
     }
@@ -31,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('lists');
     }
 }
