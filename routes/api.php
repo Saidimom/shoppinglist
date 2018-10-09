9a05a8a6f241b09c0a24efeee61e488830ff5fb9
Apollo <nitsoramo@gmail.ocm>
2018-10-09 17:25:07 +0500

    Changed readme

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
 //List of items
 Route::get('items', 'ItemsController@index')->name('api.items.index');
 
